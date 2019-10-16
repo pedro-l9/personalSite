4aeec7b7a87c608047c4b35786709c94e8cadba0
Pedro Lacerda <phenrique.lacerda1@gmail.com>
2019-10-15 22:55:07 -0300

    Adiciona tema NES e esqueleto do site

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="card nes-container with-title is-dark is-rounded is-centered">
+        <p className="title">Hello!</p>
+        <div className="card-container">
+          <div className="avatar">
+            <img
+              className="nes-avatar is-rounded"
+              alt="Avatar"
+              src="https://www.gravatar.com/avatar/1774f7db0ce16124fd9505f0e6851e6a?s=600"
+            />
+          </div>
+          <div className="presentation">
+            My name is
+            <h1>Pedro Lacerda</h1>I'm a Backend Engineer at{" "}
+            <span id="dito">Dito</span>
+          </div>
+          <div className="checkMeOut">Check me out on:</div>
+          <div className="dismiss">
+            <button
+              type="button"
+              className="nes-btn is-error"
+              onClick={() => {
+                const card = document.querySelector(".card");
+                if (card) card.classList.add("animated", "hinge");
+              }}
+            >
+              Dismiss
+            </button>
+          </div>
+          <div className="social">
+            <i className="nes-icon github is-large" />
+            <i className="nes-icon linkedin is-large" />
+            <i className="nes-icon instagram is-large" />
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
