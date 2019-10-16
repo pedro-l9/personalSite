58da6810aafa03dae7ae0ba066595a3fd00dd56d
Pedro Lacerda <phenrique.lacerda1@gmail.com>
2019-10-16 12:13:42 -0300

    Organiza melhor o CSS e adiciona os dois paineis do site

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,87 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import PresentationCard from "./components/PresentationCard/PresentationCard";
 
 import "./App.css";
 
-const App: React.FC = () => (
-  <div className="App">
-    <div className="technologies">
-      <section className="good-at nes-container is-dark with-title is-centered">
-        <p className="title">I'm really good with</p>
-        <section className="tech-list">
-          <i className="nes-icon youtube is-medium" />
-          <i className="nes-icon youtube is-medium" />
-          <i className="nes-icon youtube is-medium" />
-          <i className="nes-icon youtube is-medium" />
-          <i className="nes-icon youtube is-medium" />
-          <i className="nes-icon youtube is-medium" />
-          <i className="nes-icon youtube is-medium" />
-          <i className="nes-icon youtube is-medium" />
+const mobileWidthThreshold = 800;
+
+function handleResize(setIsMobile: Function) {
+  setIsMobile(window.innerWidth <= mobileWidthThreshold);
+}
+
+const App: React.FC = () => {
+  const [isMobile, setIsMobile] = useState(
+    window.innerWidth <= mobileWidthThreshold
+  );
+
+  const [isDismissed, setDismissed] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("resize", () => handleResize(setIsMobile));
+
+    return () =>
+      window.removeEventListener("resize", () => handleResize(setIsMobile));
+  });
+
+  return (
+    <div className="App">
+      <section
+        className={`technologies ${isDismissed ? "animated fadeOutLeft" : ""}`}
+      >
+        <section className="good-at nes-container is-dark with-title is-centered">
+          <p className="title">I'm really good with</p>
+          <section className="tech-list">
+            <i className="nes-icon youtube" />
+            <i className="nes-icon youtube" />
+            <i className="nes-icon youtube" />
+            <i className="nes-icon youtube" />
+            <i className="nes-icon youtube" />
+            <i className="nes-icon youtube" />
+            <i className="nes-icon youtube" />
+            <i className="nes-icon youtube" />
+          </section>
+        </section>
+        <section className="played-with nes-container is-dark with-title is-centered">
+          <p className="title">I've played around with</p>
+          <section className="tech-list">
+            <i className="nes-icon youtube" />
+            <i className="nes-icon youtube" />
+            <i className="nes-icon youtube" />
+            <i className="nes-icon youtube" />
+            <i className="nes-icon youtube" />
+            <i className="nes-icon youtube" />
+            <i className="nes-icon youtube" />
+          </section>
+        </section>
+        <section className="will-learn nes-container is-dark with-title is-centered">
+          <p className="title">I plan on learning</p>
+          <section className="tech-list">
+            <i className="nes-icon youtube" />
+            <i className="nes-icon youtube" />
+            <i className="nes-icon youtube" />
+            <i className="nes-icon youtube" />
+            <i className="nes-icon youtube" />
+            <i className="nes-icon youtube" />
+            <i className="nes-icon youtube" />
+          </section>
         </section>
       </section>
-      <section className="played-with nes-container is-dark with-title is-centered">
-        <p className="title">I've played around with</p>
-        <section className="tech-list">
-          <i className="nes-icon youtube is-medium" />
-          <i className="nes-icon youtube is-medium" />
-          <i className="nes-icon youtube is-medium" />
-          <i className="nes-icon youtube is-medium" />
-          <i className="nes-icon youtube is-medium" />
-          <i className="nes-icon youtube is-medium" />
-          <i className="nes-icon youtube is-medium" />
-        </section>
-      </section>
-      <section className="will-learn nes-container is-dark with-title is-centered">
-        <p className="title">I plan on learning</p>
-        <section className="tech-list">
-          <i className="nes-icon youtube is-medium" />
-          <i className="nes-icon youtube is-medium" />
-          <i className="nes-icon youtube is-medium" />
-          <i className="nes-icon youtube is-medium" />
-          <i className="nes-icon youtube is-medium" />
-          <i className="nes-icon youtube is-medium" />
-          <i className="nes-icon youtube is-medium" />
+      <PresentationCard
+        isMobile={isMobile}
+        isDismissed={isDismissed}
+        dismiss={() => setDismissed(true)}
+      />
+      <section
+        className={`acomplishments ${
+          isDismissed ? "animated fadeOutRight" : ""
+        }`}
+      >
+        <section className="acomplishments-container nes-container is-dark with-title is-centered">
+          <p className="title">Acomplishments</p>
         </section>
       </section>
     </div>
-    <PresentationCard />
-    <div className="acomplishments"></div>
-  </div>
-);
+  );
+};
 export default App;
--- src/components/PresentationCard/PresentationCard.tsx
+++ src/components/PresentationCard/PresentationCard.tsx
@@ -2,63 +2,73 @@ import React from "react";
 
 import "./PresentationCard.css";
 
-function dismissCard() {
-  const card = document.querySelector(".card");
-  if (card) card.classList.add("animated", "hinge");
-}
-
-const PresentationCard = () => (
-  <div className="card nes-container with-title is-dark is-rounded is-centered">
-    <p className="title">Hello, world!</p>
-    <div className="card-container">
-      <div className="avatar">
-        <img
-          className="nes-avatar is-rounded"
-          alt="Avatar"
-          src="https://www.gravatar.com/avatar/1774f7db0ce16124fd9505f0e6851e6a?s=600"
-        />
-      </div>
-      <div className="presentation">
-        My name is
-        <h1>Pedro Lacerda</h1>I'm a Backend Engineer at{" "}
-        <span id="dito">
+const PresentationCard = (props: {
+  isMobile: boolean;
+  isDismissed: boolean;
+  dismiss: Function;
+}) => {
+  return (
+    <div
+      className={`
+        card nes-container with-title is-dark is-centered 
+        ${props.isMobile ? "" : "is-rounded"} 
+        ${props.isDismissed ? "animated hinge" : ""}
+      `}
+    >
+      <p className="title">Hello, world!</p>
+      <div className="card-container">
+        <div className="avatar">
+          <img
+            className="nes-avatar is-rounded"
+            alt="Avatar"
+            src="https://www.gravatar.com/avatar/1774f7db0ce16124fd9505f0e6851e6a?s=600"
+          />
+        </div>
+        <div className="presentation">
+          My name is
+          <h1>Pedro Lacerda</h1>{" "}
+          <span>
+            I'm a Backend Engineer at{" "}
+            <span id="dito">
+              <a
+                href="https://dito.com.br"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Dito
+              </a>
+            </span>
+          </span>
+        </div>
+        <div className="checkMeOut">Check me out on:</div>
+        <div className="dismiss">
+          <button
+            type="button"
+            className="nes-btn is-error"
+            onClick={() => props.dismiss()}
+          >
+            Dismiss
+          </button>
+        </div>
+        <div className="social">
           <a
-            href="https://dito.com.br"
+            href="https://github.com/pedro-l9"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Dito
+            <i className="nes-icon github is-large" />
           </a>
-        </span>
-      </div>
-      <div className="checkMeOut">Check me out on:</div>
-      <div className="dismiss">
-        <button
-          type="button"
-          className="nes-btn is-error"
-          onClick={dismissCard}
-        >
-          Dismiss
-        </button>
-      </div>
-      <div className="social">
-        <a
-          href="https://github.com/pedro-l9"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <i className="nes-icon github is-large" />
-        </a>
-        <a
-          href="https://www.linkedin.com/in/phenriquel/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <i className="nes-icon linkedin is-large" />
-        </a>
+          <a
+            href="https://www.linkedin.com/in/phenriquel/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i className="nes-icon linkedin is-large" />
+          </a>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default PresentationCard;
