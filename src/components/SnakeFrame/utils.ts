9a0dfef0764e24db61dac8b12949c707dcc7679f
Pedro Lacerda <phenrique.lacerda1@gmail.com>
2020-08-18 15:14:45 -0300

    Bump da versao do functional-snake

--- src/components/SnakeFrame/utils.ts
+++ src/components/SnakeFrame/utils.ts
@@ -7,7 +7,7 @@ export const getPositionInPlane = (
 ) => (pixel: number) => Math.round((pixel * planeSize) / divisions) - padding;
 
 export function isGameFinished(frame: Frame) {
-  return frame.gameOver && frame.snake.length === 1;
+  return frame.hasSnakeCrashed && frame.snake.length === 1;
 }
 
 export function getInputForKey(key: string): Input {
